b08e53aff0e9970d9e90893e4a594f41812bcf35
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-09-06 16:58:35 +0300

    added test.pug and public dir

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
+const path       = require( 'path'        );
 const express    = require( 'express'     );
 const bodyParser = require( 'body-parser' );
+
+const rootDir = require( './utils/rootDir' );
 
 const app = express();
 
 app.set( 'view engine', 'pug' );
 app.set( 'views', 'views'     );
 
-app.use( express.static() );
-//process.mainModule.filename
+app.use( express.static( path.join( rootDir, 'public' ) ) ); 
 app.use( bodyParser.urlencoded( { extended: false } ) );
 
 app.get( '/', ( req, res, next ) => {
-    res.write( 'Hello world!' );
-    res.end();
+    res.render( 'test' );
 } );
 
 app.listen( process.env.PORT || 3000 );
