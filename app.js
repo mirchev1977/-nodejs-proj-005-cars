ed8e8525971bc9ad844c50aed7efb7fcc127545d
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-09-06 14:52:50 +0300

    set view engine and views; use bodyParser.urlencoded

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const express = require( 'express' );
+const express    = require( 'express'     );
+const bodyParser = require( 'body-parser' );
+
 const app = express();
+
+app.set( 'view engine', 'pug' );
+app.set( 'views', 'views'     );
+
+app.use( bodyParser.urlencoded( { extended: false } ) );
 
 app.get( '/', ( req, res, next ) => {
     res.write( 'Hello world!' );
