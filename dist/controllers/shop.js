f64d68bdae86cdfc2cced0eae99460448c591ab4
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-09-13 04:49:43 +0300

    Display the sort criteria in the select dropdown

--- dist/controllers/shop.js
+++ dist/controllers/shop.js
@@ -6,7 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Car_1 = __importDefault(require("../entities/Car"));
 function getAllCars(req, res, next) {
     Car_1.default.fetchAll(req.query['sort']).then(_arrCars => {
-        res.render('shop/all-cars', { _arrCars: _arrCars });
+        res.render('shop/all-cars', {
+            _arrCars: _arrCars,
+            sortBy: req.query['sort']
+        });
     });
 }
 exports.getAllCars = getAllCars;
--- src/controllers/shop.ts
+++ src/controllers/shop.ts
@@ -3,6 +3,9 @@ import Car from '../entities/Car';
 
 export function getAllCars (  req, res, next  ) {
     Car.fetchAll( req.query[ 'sort' ] ).then( _arrCars => {
-        res.render( 'shop/all-cars', { _arrCars: _arrCars } );
+        res.render( 'shop/all-cars', { 
+            _arrCars: _arrCars,
+            sortBy:   req.query[ 'sort' ] 
+        } );
     } ); 
 }
